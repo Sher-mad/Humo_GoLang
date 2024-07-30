0d42119e5d7db44888ed984f8eeef60bee1ef5b6
Sher-mad <sherzodqurbonov43@gmail.com>
2024-07-30 19:11:29 +0500

    Добавка задач в папке HW8

--- HW8/Func.go
+++ HW8/Func.go
@@ -32,3 +32,87 @@ func findMinArr(arr []int) int {
 	}
 	return max
 }
+
+/*
+3.	Подсчитать количество положительных чисел в массиве.
+*/
+func findCheck(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+	check := 0
+	for _, chec := range arr {
+		if chec > 0 {
+			check++
+		}
+
+	}
+	return check
+
+}
+
+/*
+4. Найти сумму всех элементов массива.
+*/
+func AmountArr(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+
+	amountMax := 0
+	for _, num := range arr {
+		amountMax += num
+	}
+	return amountMax
+}
+
+/*
+5. Найти среднее значение всех элементов массива.
+*/
+func findAverage(average []float64) float64 {
+	if len(average) == 0 {
+		return 0.0
+	}
+	var sum float64
+	for _, v := range average {
+		sum += v
+	}
+	return sum / float64(len(average))
+}
+
+/*
+6. Удалить все вхождения заданного числа из массива.
+*/
+func deleteElementArr(arr []int, delete int) ([]int, []int) {
+	if len(arr) == 0 {
+		return arr, arr
+	}
+	slow := 0
+	// for i := 0; i < len(arr); i++ {
+	// 	if arr[i] != delete {
+	// 		arr[slow] = arr[i]
+	// 		slow++
+	// 	}
+	// }
+	// return arr[:slow]
+
+	for i, v := range arr {
+		if v != delete {
+			arr[slow] = arr[i]
+			slow++
+		}
+	}
+	return arr[:slow], arr
+
+}
+
+// 7. Умножить все элементы массива на заданное число.
+
+func multiplyArray(num []int, multiply int) []int {
+	zero := []int{0}
+	if len(num) == 0 {
+		return zero
+	}
+	return num
+	
+}
--- HW8/main.go
+++ HW8/main.go
@@ -12,9 +12,41 @@ func main() {
 
 	// 2. Найти минимальный элемент в массиве.
 	fmt.Println("2. Найти минимальный элемент в массиве.")
-	numbersMin := []int{3, 5, 9, 6, 7, 2}
-	resultMinArrMax := findMinArr(numbersMin)
-	fmt.Println("Arr = ", numbersMin)
+	numbersArr2 := []int{3, 5, 9, 6, 7, 2}
+	resultMinArrMax := findMinArr(numbersArr2)
+	fmt.Println("Arr = ", numbersArr2)
 	fmt.Println("Min arr = ", resultMinArrMax)
+
+	// 3. Подсчитать количество положительных чисел в массиве.
+	fmt.Println("3. Подсчитать количество положительных чисел в массиве.")
+	numbersArr3 := []int{3, -5, 9, -6, 7, -2}
+	resultFindArrCheck := findCheck(numbersArr3)
+	fmt.Println("Arr = ", numbersArr3)
+	fmt.Println("Check = ", resultFindArrCheck, "столько положттелных чисел")
+
+	// 4. Найти сумму всех элементов массива.
+	fmt.Println("4. Найти сумму всех элементов массива.")
+	numbersArr4 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	resultSumArr := AmountArr(numbersArr4)
+	fmt.Println("Array = ", numbersArr4)
+	fmt.Println("Sum Array = ", resultSumArr)
+
+	// 5. Найти среднее значение всех элементов массива.
+	fmt.Println("5. Найти среднее значение всех элементов массива.")
+	numbersArr5 := []float64{1.5, 3.5, 5.5, 7.5, 9.5, 2.5, 6.5}
+	resultArr5 := findAverage(numbersArr5)
+	fmt.Println("Array 5 = ", numbersArr5)
+	fmt.Printf("Averge = %.3f", resultArr5)
+	fmt.Println("Averge = ", resultArr5)
+
+	// 6. Удалить все вхождения заданного числа из массива.
+	fmt.Println("6. Удалить все вхождения заданного числа из массива.")
+	numbersArr6 := []int{2, 1, 2, 7, 3, 5, 2, 4, 2}
+	fmt.Println("Array 6 = ", numbersArr6)
+	dele := 2
+	resultArr6, resultArr7 := deleteElementArr(numbersArr6, dele)
+	// fmt.Println("Array 6 = ", numbersArr6)
+	fmt.Println("Из масифа удалени элементи ", dele, "\n After", resultArr6)
+	fmt.Println("Из масифа удалени элементи ", dele, "\n After", resultArr7)
 
 }
